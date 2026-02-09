40ff121c59073f67245830d7b7f9f41d9242a101
alberto.lopes <cast.alberto@pe.sebrae.com.br>
2026-02-09 19:46:50 -0300

    feat: ajustando rodape

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -4,11 +4,11 @@ import testIds from '@app/utils/test-ids';
 
 const Footer = () => (
   <footer
-    className="m-h-56 leading-7 sm:p-14 font-site"
+    className="m-h-56 leading-7 sm:px-14 sm:pt-14 font-site"
     data-testid={testIds.LAYOUT.FOOTER}
   >
     <div className="flex flex-col sm:flex-row">
-      <div className="basis-2/3 bg-orange-500 text-white p-14 sm:pl-44">
+      <div className="w-full bg-orange-500 text-white p-14 sm:pl-44">
         <h2 className="text-2xl sm:text-3xl font-bold">Fale Conosco</h2>
         <div className="flex flex-col sm:flex-row text-sm font-helvetica">
           <div className="basis-1/3 border-b border-white pb-4">
@@ -28,25 +28,10 @@ const Footer = () => (
           </div>
         </div>
       </div>
-      <div className="basis-1/3 bg-gray-200 p-14 text-center">
-        <h2 className="text-2xl sm:text-3xl font-bold">Fique Informado</h2>
-        <p className="mt-6 text-slate-500">
-          Cadastre seu e-mail e receba novidades, informações e conteúdos sobre
-          esclerose múltipla.
-        </p>
-        <input
-          type="email"
-          className="my-6 w-3/4 block mx-auto bg-transparent border-0 border-b border-orange-500 text-orange-500"
-          placeholder="Email Address"
-        />
-        <a href="" className="text-slate-500 py-6 font-site">
-          Inscreva-se
-        </a>
-      </div>
     </div>
-    <div className="mx-auto text-center sm:text-xs mt-6">
+    <div className="mx-auto text-center sm:text-xs mt-4">
       {/*<Logo />*/}
-      <p className="font-default mb-10">
+      <p className="font-default mb-4 text-slate-500">
         © 2025 Apemigos. Desenvolvido por{' '}
         <a
           href="https://github.com/albertolopes"
